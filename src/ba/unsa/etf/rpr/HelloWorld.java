0cc58422402ae98b1bac1a1743aa49e519a791a8
Sanina <sherak1@etf.unsa.ba>
2021-11-12 02:24:16 +0100

    helloo

--- src/ba/unsa/etf/rpr/HelloWorld.java
+++ src/ba/unsa/etf/rpr/HelloWorld.java
@@ -1,8 +1,8 @@
 package ba.unsa.etf.rpr;
 
-public class Main {
+public class HelloWorld {
 
     public static void main(String[] args) {
-
+    System.out.println ("Dobro došli na RPR");
     }
 }
